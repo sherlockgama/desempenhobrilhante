3f0e58408d19a88fa9a295f6162e024050d1d2f7
Marcelo Leandro Pereira <marceloleandropereira@gmail.com>
2019-04-30 20:29:27 -0300

    Refactoring

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDO5AJic1epcU7IgtYQ4dzJpKoGPBKy0RE",
+    authDomain: "desempenhobrilhantegama.firebaseapp.com",
+    databaseURL: "https://desempenhobrilhantegama.firebaseio.com",
+    projectId: "desempenhobrilhantegama",
+    storageBucket: "desempenhobrilhantegama.appspot.com",
+    messagingSenderId: "732956392359"
+};
+firebase.initializeApp(config);
