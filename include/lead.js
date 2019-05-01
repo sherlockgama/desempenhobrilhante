aace76057859258346f71dac11f86ea8c5a97761
Marcelo Leandro Pereira <marceloleandropereira@gmail.com>
2019-04-30 21:50:06 -0300

    Listagem de artigos, adicionado includes para não repetir código

---
+++ include/lead.js
@@ -0,0 +1,55 @@
+var ip = 0;
+
+$.ajax({
+    type: 'GET',
+    url: 'https://api.ipify.org?format=json',
+}).done(function(data) { 
+    ip = data.ip;
+});
+
+$('.toast').toast({
+    animation: true,
+    autohide: true,
+    delay: 1000
+});
+
+var emailInput = $('#emailInput');
+var nameInput = $('#nameInput');
+var emailInput2 = $('#emailInput2');
+var nameInput2 = $('#nameInput2');
+
+$('#submit').click(function () {
+    checkSubmit(nameInput, emailInput);
+})
+
+$('#submit2').click(function () {
+    checkSubmit(nameInput2, emailInput2);
+})
+
+function checkSubmit(inputName, inputEmail) {
+    if (!inputName.val()) {
+        alert("Informe o nome");
+        inputName.focus();
+        return false;
+    } else if (!inputEmail.val()) {
+        alert("Informe o email");
+        inputEmail.focus();
+        return false;
+    }
+    create(inputEmail.val(), inputName.val(), ip);
+    inputName.prop('disabled', true);
+    inputEmail.prop('disabled', true);
+}
+
+function create(email, name, ip) {
+    var lead = {
+        email: email,
+        nome: name,
+        ip: ip,
+        tipo: 'B2B',
+        data_hora: new Date().toJSON()
+    }
+
+    firebase.database().ref().child('leads').push(lead);
+    alert('Inscrição realizada');
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <?php include("include/head.html") ?>
+    </head>
+    <body>        
+        <?php include("include/header.html") ?>
+
+        <div class="container">
+            <div class="card flex-row flex-wrap">
+                <div class="card-header border-0 col-4">
+                    <img src="img/gestao/dream_team.jpg" width="330px">
+                </div>
+                <div class="card-block px-3 col-8">
+                    <span class="list-card-category">Gestão</span>
+                    <p class="list-card-title">Descubra como ter um <i>dream team</i></p>
+                    <p class="list-card-text">Todo gestor possui dificuldades na direção de um time, sendo o trabalho em equipe um dos principais desafios no seu dia a dia. E tal tarefa desencadeia inúmeros problemas, como atingimento de metas, entrega nos prazos corretos, qualidade dos projetos e satisfação dos stakeholders. </p>
+                    <p class="list-card-date">30 de abril de 2019</p>
+                </div>
+                <div class="w-100"></div>
+            </div>
+        </div>
+
+        <?php //include("include/artigos_relacionados.html") ?>
+
+        <?php include('include/footer.html') ?>
+    </body>
+    <script type="text/javascript" src="include/lead.js"></script>
+    
+</html>
