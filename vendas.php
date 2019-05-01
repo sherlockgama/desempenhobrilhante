25f1973d3d19cbb3b701e67a6a677fb67091673e
Marcelo Leandro Pereira <marceloleandropereira@gmail.com>
2019-05-01 04:52:03 -0300

    Adição das páginas de categorias, primeiro artigo de gestão, meta tags, chatbot, analytics, código para remarketing de facebook ads, linkedin

---
+++ vendas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <?php include("include/head.html") ?>
+    </head>
+    <body>        
+        <?php include("include/header.html") ?>
+
+        <div class="container">
+            :(
+        </div>
+
+        <?php //include("include/artigos_relacionados.html") ?>
+
+        <?php include('include/footer.html') ?>
+    </body>
+    <script type="text/javascript" src="include/lead.js"></script>
+    
+</html>
