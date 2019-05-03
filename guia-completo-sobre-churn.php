01d86affbd6b54e24cb42ce3a7c745f112c23747
Marcelo Leandro Pereira <marceloleandropereira@gmail.com>
2019-05-03 01:36:41 -0300

    Link para o e-book

---
+++ guia-completo-sobre-churn.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <?php include("include/head.html") ?>
+        <link rel="stylesheet" href="css/churn.css">
+    </head>
+    <body>
+        <div class="flex-align"><
+            <div class="container">
+                <div class="row">
+                    <div class="wrapper col-5">
+                        <div>
+                            <p class="title">Guia completo sobre Churn Rate</p>
+                            <p class="text text-justify">
+                                Entenda o que é o Churn Rate, como ele impacta seu negócio e como você pode estruturar um processo inteligente que vai te ajudar a reduzir esse indicador.
+                            <p>
+                        </div>
+                    </div>
+                    <div class="col-7">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript" src="include/lead.js"></script>
+    
+</html>
