53f8eb6e2dc0bc0e63c58043354c8af969dfb6ca
Marcelo Leandro Pereira <marceloleandropereira@gmail.com>
2019-05-09 04:19:18 -0300

    Landing page do evento Gestão em Foco

---
+++ evento-gestao-em-foco.php
@@ -0,0 +1,194 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+    <meta charset="utf-8"/>
+    <title>Desempenho Brilhante</title>
+
+    <link rel="icon" href="img/icon/favicon.png" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/event.css">
+    <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.10.1/firebase.js"></script>
+    <script type="text/javascript" src="js/firebase.js"></script>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="description" content="Melhores conteúdos para você melhorar ainda mais a Gestão da sua equipe, TI, RH e vendas!">
+    <meta name="keywords" content="Evento,Gestão,Foco,Curitiba,Desempenho">
+    <meta content="Desempenho Brilhante" name="author" />
+
+    <meta property="og:title" content="Blog Desempenho Brilhante ">
+    <meta property="og:type" content="article">
+    <meta property="og:image" content="http://desempenhobrilhante.com.br/img/logo/DESEMPENHO_BRILHANTE_LOGO.jpg">
+    <meta property="og:url" content="http://desempenhobrilhante.com.br/">
+    <meta property="og:description" content="Evento Gestão em Foco">
+    </head>
+    <body>        
+        <script>
+            // Initialize Firebase
+            var config = {
+                apiKey: "AIzaSyDO5AJic1epcU7IgtYQ4dzJpKoGPBKy0RE",
+                authDomain: "desempenhobrilhantegama.firebaseapp.com",
+                databaseURL: "https://desempenhobrilhantegama.firebaseio.com",
+                projectId: "desempenhobrilhantegama",
+                storageBucket: "desempenhobrilhantegama.appspot.com",
+                messagingSenderId: "732956392359"
+            };
+            firebase.initializeApp(config);
+        </script>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-sm-12">
+                    <a href="index.php"><img src="img/logo/DESEMPENHO_BRILHANTE_LOGO.jpg" height="120px"></a>
+                    <div class="social-icons">
+                        <a href="https://www.facebook.com/Desempenho-Brilhante-351769538875917/" target="_blank"><img src="img/icon/facebook.png" height="15px"></a>
+                        <a href="https://www.instagram.com/desempenhobrilhante/" target="_blank"><img src="img/icon/instagram.png" height="15px"></a>
+                    </div>
+                </div>
+                <form class="form-inline">
+                    <input type="text" class="form-control mb-2 mr-sm-2 header-input" id="nameInput" placeholder="Digite seu nome">
+                    <div class="input-group mb-2 mr-sm-2">
+                        <input type="text" class="form-control header-input" id="emailInput" placeholder="Digite seu email">
+                    </div>
+                    <span type="button" class="btn btn-blog mb-2" id="submit">Receber notícias</span>
+                </form>
+            </div>
+
+            <div class="row banner" style="margin-top: 20px">
+                <div class="col-md-12 col-sm-12">
+                    <img src="img/BANNER_SITE_1080x320px.png" alt="Gestão em Foco" class="col-md-12 col-sm-12">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <p class="event-title"><b>O MELHOR DA GESTÃO É AQUI</b></p>
+                    <p>
+                        Nosso evento acontecerá em <font color="#FF9D00">Curitiba</font> e reunirá especialistas em novos métodos e ferramentas de gestão e qualidade. Para garantir seu lugar, basta clicar no Banner acima ou <a href="https://www.sympla.com.br/gestao-em-foco-cwb__521903" target="_blank">nesse link.</a>
+                    </p>
+                    <div class="row" style="margin-top: 20px">
+                        <div class="col-md-8 col-sm-12">
+                            <img src="img/evento/banners_site_interna-01.png" alt="Gestão em Foco" class="col-md-12 col-sm-12">
+                        </div>
+                    </div>
+                    <p>
+                        <h3>Sua empresa mais ágil com Lean e Agile</h3>
+                    </p>
+                    <p class="text-justify">
+                        Beatriz é UX Designer com vasto background em Branding e Design de Serviços. Atualmente trabalha na startup Tecnofit. Trabalhou gerenciando e analisando equipes com foco em Customer Success. Utiliza metodologia Lean e Agile para alavancar a produtividade em projetos e garantir o sucesso do cliente.
+                    </p>
+                    <div class="row" style="margin-top: 20px">
+                        <div class="col-md-8 col-sm-12">
+                            <img src="img/evento/banners_site_interna-02.png" alt="Gestão em Foco" class="col-md-12 col-sm-12">
+                        </div>
+                    </div>
+                    <p>
+                        <h3>Ferramentas para melhorar sua gestão: Kaizen e 5S</h3>
+                    </p>
+                    <p class="text-justify">
+                        Luz Alba foi professora na UFRJ e agora é sócia na DNA Consulting, escritório especializado em Marketing Jurídico. Possui MBA em gestão na Faculdade Inspirar e trabalha em Gestão de Qualidade. Seu foco é aumentar a produtividade do seu time usando as mais modernas ferramentas de gestão.
+                    </p>
+
+                    <div class="row" style="margin-top: 20px">
+                        <div class="col-md-8 col-sm-12">
+                            <img src="img/evento/banners_site_interna-01.png" alt="Gestão em Foco" class="col-md-12 col-sm-12">
+                        </div>
+                    </div>
+                    <p>
+                        <h3>
+                            Inteligência Organizacional                            
+                        </h3>
+                    </p>
+                    <p class="text-justify">
+                        Marco é Doutor pela UFSC e Mestre em Educação em gestão de sistemas e IES pela PUCPR. Atualmente é professor com experiências na área de Administração, Ergonomia, Gestão da Informação e Fisioterapia, com ênfase em Saúde do Trabalhador.
+                    </p>
+                    <div class="row" style="margin-top: 20px">
+                        <div class="col-md-8 col-sm-12">
+                            <img src="img/evento/banners_site_interna-02.png" alt="Gestão em Foco" class="col-md-12 col-sm-12">
+                        </div>
+                    </div>
+                    <p>
+                        <h3>
+                            Rodada de Negócios                            
+                        </h3>
+                    </p>
+                    <p class="text-justify">
+                        Essa é a sua oportunidade de mostrar e falar sobre o seu negócio. Apresente-se, diga como sua empresa funciona ou como vai melhorá-la. É o seu momento de fazer Networking.
+                    </p>
+                    <p class="event-message">
+                        Se identificou? Então venha compartilhar conosco:
+                    </p>
+                    <p class="event-message">
+                        Local: Auditório do Batel Office Tower (3 andar) </br>
+                        Quinta-feira, 09 de maio de 2019 </br>
+                        Horário às 19:00
+                    <p>
+                    <a href="https://www.sympla.com.br/gestao-em-foco-cwb__521903" target="_blank"><span type="button" class="btn btn-blog mb-2" id="submit">Garanta seu Ingresso</span></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-md-12 footer-register col-sm-12">
+            <div class="footer-register-title">
+                <span class="footer-title">Receba mais conteúdos como esse!</span>
+                <p class="footer-subtitle">Inscreva-se abaixo e fique atualizado com o melhor conteúdo sobre gestão e produtividade</p>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <form class="form-inline justify-content-center">
+                        <input type="text" class="form-control mb-2 mr-sm-2" id="nameInput2" placeholder="Insira seu nome">
+                        <div class="input-group mb-2 mr-sm-2">
+                            <input type="text" class="form-control email-input" id="emailInput2" placeholder="Insira seu endereço de email">
+                        </div>
+                        <span type="button" class="btn btn-blog mb-2" id="submit2">Inscrever-se</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 overlay">
+                    <a href="index.php"><img class="img-logo" src="img/logo/DESEMPENHO_BRILHANTE_LOGO.jpg"></a>
+                    <p class="footer-text">Blog Desempenho Brilhante, informando e ajudando você a conseguir um melhor desempenho e mais produtividade</p>
+                </div>
+                <div class="col-md-5 col-sm-12">
+                    <div class="footer-social">
+                        <span class="footer-text">Siga nossas outras mídias sociais</span>
+                        <div class="row justify-content-center footer-social-icons">
+                            <a href="https://www.facebook.com/Desempenho-Brilhante-351769538875917/" target="_blank"><img src="img/icon/facebook.png" height="15px"></a>
+                            <a href="https://www.instagram.com/desempenhobrilhante/" target="_blank"><img src="img/icon/instagram.png" height="15px"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <div class="row footer-links">
+                        <div class="col-md-4 offset-md-4 col-sm-12">
+                            <b>Temas</b>
+                            <a href="produtividade.php" class="decoration-none"><span>Produtividade</span></a>
+                            <a href="gestao.php" class="decoration-none"><span>Gestão</span></a>
+                            <a href="vendas.php" class="decoration-none"><span>Vendas</span></a>
+                            <a href="evento-gestao-em-foco.php" class="decoration-none"><span>Evento</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="copyright footer-text col-md-12 col-sm-12">
+                Copyright Sherlock. 2019
+            </div>
+        </div>
+    </div>
+    <div id="widget-sympla">
+        <iframe src="https://www.sympla.com.br/tickets-grid-widget?eid=521903&lang=pt" height="180px" frameborder="0" scrolling="no"></iframe>
+    </div>
+    </body>
+    <script type="text/javascript" src="js/lead.js"></script>
+</html>
